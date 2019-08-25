f33d8dea547b9e0057efd579a0c91d68ea3d0406
DmonicAct <lck_23@hotmail.com>
2019-08-24 23:40:01 -0500

    second commit

---
+++ src/main/java/com/aevent/aevent/AeventApplication.java
@@ -0,0 +1,13 @@
+package com.aevent.aevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aevent/aevent/AeventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aevent.aevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
