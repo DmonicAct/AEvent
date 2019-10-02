deb986f8c43b089e5dd27ea44f9425a69ed4b752
alulab14 <alulab14@inf.pucp.edu.pe>
2019-10-02 18:16:39 -0500

    terminado api parametro

---
+++ src/main/java/com/pucp/aevent/dao/IParametroDao.java
@@ -0,0 +1,10 @@
+package com.pucp.aevent.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pucp.aevent.entity.Parametro;
+public interface IParametroDao extends JpaRepository <Parametro, Long>{
+	public List<Parametro> findAll();
+}
---
+++ src/main/java/com/pucp/aevent/service/IParametroService.java
@@ -0,0 +1,15 @@
+package com.pucp.aevent.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+
+import com.pucp.aevent.entity.Parametro;
+import com.pucp.aevent.entity.response_objects.Error;
+import com.pucp.aevent.entity.response_objects.Paginacion;
+
+public interface IParametroService {
+	public List<Parametro> findAll(Pageable pageable);
+	Paginacion getPaginacion();
+	Error getError();
+}
