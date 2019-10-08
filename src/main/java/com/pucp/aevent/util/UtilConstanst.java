1ef0d1010f4a42add94ec8d9953fc512db86c8c0
DmonicAct <lck_23@hotmail.com>
2019-10-08 11:25:25 -0500

    constants, util

--- src/main/java/com/pucp/aevent/util/UtilConstanst.java
+++ src/main/java/com/pucp/aevent/util/UtilConstanst.java
@@ -11,5 +11,20 @@ public class UtilConstanst {
 	public static final String  APP_ORIGIN_HEADERS_TYPE = "Authorization";
 	
 	//Routes Common All
-	public static final String APP_ROUTE_FULL_ACCESS_EMAIL = "/api/usuarios/email/**";
+	public static final String APP_ROUTE_OUT_ACCESS= "/api/usuarios/out/**";
+	
+	//Roles
+
+	public static final long ROLE_ADMIN = 1;
+	public static final long ROLE_PRESIDENT = 2;
+	public static final long ROLE_ORGANIZER = 3;
+	public static final long ROLE_EVALUATOR = 4;
+	public static final long ROLE_USER = 5;
+	
+	public static final String NAME_ROLE_ADMIN = "ROLE_ADMIN";
+	public static final String NAME_ROLE_PRESIDENT = "ROLE_PRESIDENT";
+	public static final String NAME_ROLE_ORGANIZER = "ROLE_ORGANIZER";
+	public static final String NAME_ROLE_EVALUATOR = "ROLE_EVALUATOR";
+	public static final String NAME_ROLE_USER = "ROLE_USER";
+
 }
