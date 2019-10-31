b9581e3855c06ff883e2e9b9f93e4481e7135747
DmonicAct <lck_23@hotmail.com>
2019-10-30 19:56:09 -0500

    IMPORTS

--- src/main/java/com/pucp/aevent/entity/Criterio.java
+++ src/main/java/com/pucp/aevent/entity/Criterio.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -12,6 +13,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
