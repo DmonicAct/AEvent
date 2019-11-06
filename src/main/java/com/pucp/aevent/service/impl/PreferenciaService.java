3fcc700e58df71e58265e0a40ab408a1d49303e8
WillyChung <48967729+WillyChung@users.noreply.github.com>
2019-11-06 02:40:25 -0500

    Preferencia-Willy

---
+++ src/main/java/com/pucp/aevent/service/impl/PreferenciaService.java
@@ -0,0 +1,37 @@
+package com.pucp.aevent.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pucp.aevent.dao.IPersonaDao;
+import com.pucp.aevent.dao.IPreferenciaDao;
+import com.pucp.aevent.entity.Preferencia;
+import com.pucp.aevent.entity.Propuesta;
+import com.pucp.aevent.entity.Usuario;
+import com.pucp.aevent.service.IPreferenciaService;
+
+@Service
+public class PreferenciaService implements IPreferenciaService {
+	@Autowired
+	IPersonaDao daoPersona;
+	@Autowired
+	IPreferenciaDao dao;
+/*	@Override
+	public List<Preferencia> findByUsuario(Usuario user) {
+		return dao.findByUsuario(user);
+	}*/
+	@Override
+	public List<Preferencia> findByIdUsuario(int idUsuario) {
+		return dao.findByIdUsuario(idUsuario);
+	}
+	
+	
+/*
+	@Override
+	public Preferencia findByUsuarioAndPropuesta(Usuario user,Propuesta prop) {
+		return dao.findByUsuarioAndPropuesta(user,prop);
+	}*/
+
+}
