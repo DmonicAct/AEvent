093e3268877acff72d0f466f4eb8b383e9050095
DmonicAct <lck_23@hotmail.com>
2019-11-09 10:51:57 -0500

    Ingreso de Constantes, para estado de evento

--- src/main/java/com/pucp/aevent/util/UtilConstanst.java
+++ src/main/java/com/pucp/aevent/util/UtilConstanst.java
@@ -24,11 +24,12 @@ public class UtilConstanst {
 	public static final String NAME_ROLE_DAFAULT = "ROLE_DAFAULT";
 	
 	//Estado Evento
-	public static final String EVENTO_BORRADOR		= "EVENTO_BORRADOR";
-	public static final String EVENTO_CANCELADO		= "EVENTO_CANCELADO";
-	public static final String EVENTO_PRESIDENTE  	= "EVENTO_PRESIDENTE";
-	public static final String EVENTO_PONENCIA 		= "EVENTO_BORRADOR";
-	public static final String EVENTO_INICIO 		= "EVENTO_BORRADOR";
+	public static final String EVENTO_FASE_BORRADOR					= "EVENTO_BORRADOR";
+	public static final String EVENTO_FASE_PRESIDENTE  				= "EVENTO_PRESIDENTE";
+	public static final String EVENTO_FASE_SELECCION_PONENCIA 		= "EVENTO_SELECCION";
+	public static final String EVENTO_FASE_INICIO 					= "EVENTO_INICIO";
+	
+	public static final String EVENTO_CANCELADO						= "EVENTO_CANCELADO";
 	//Estado Propuesta
 	public static final String PROPUESTA_ESPERA    	=	"PROPUESTA_ESPERA";	
 	public static final String PROPUESTA_APROBADA  	=	"PROPUESTA_APROBADA";	
