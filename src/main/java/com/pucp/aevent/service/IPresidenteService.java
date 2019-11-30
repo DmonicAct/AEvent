d294fface26a1ac8daf5d1619f0dd675fcf302d1
StevenAlonso <alonso.labajos@pucp.pe>
2019-11-30 02:53:47 -0500

    CALIFICACION DEL PRESIDENTE /presidente/aprobar (mandar idPostulacion) -> ACTUALIZA A POSTULACION_APROBADA Y AVANZA FASE ACTUAL DE POSTULACION Y PROPUESTA, SI ES LA ULTIMA FASE .. TAMBIEN ACTUALIZA A PROPUESTA_APROBADA. /presidente/desaprobar (mandar idPostulacion) -> RECHAZA POSTULACION Y POR ENDE RECHAZA PROPUESTA TAMBIEN, NO AVANZA FASE.

--- src/main/java/com/pucp/aevent/service/IPresidenteService.java
+++ src/main/java/com/pucp/aevent/service/IPresidenteService.java
@@ -1,9 +1,9 @@
 package com.pucp.aevent.service;
 
-import java.util.List;
-
 import com.pucp.aevent.entity.request_objects.PostulacionPropuestaRequest;
 
 public interface IPresidenteService {
 	public PostulacionPropuestaRequest obtenerPostulacionesEnEsperaYPropuestas(int idPresidente);
+	public void DesaprobarPostulacion(Long idPostulacion);
+	public void AprobarPostulacion(Long idPostulacion);
 }
